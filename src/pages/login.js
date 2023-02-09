15fc8a5e24d199587fe586ec30d2fa64d3ed69e6
Thalita <thalitacostasantos12@gmail.com>
2023-02-09 18:59:36 -0300

    Requisito 2 - feito

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,7 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchToken } from '../services/apiTrivia';
 // import logo from './trivia.png';
 
 class Login extends Component {
@@ -24,6 +27,14 @@ class Login extends Component {
       { [name]: value },
       () => this.validateInputs(),
     );
+  };
+
+  handleClick = async () => {
+    const data = await fetchToken();
+    const { token } = data;
+    localStorage.setItem('token', token);
+    const { history } = this.props;
+    history.push('/game');
   };
 
   render() {
@@ -61,6 +72,7 @@ class Login extends Component {
             type="button"
             data-testid="btn-play"
             disabled={ isDisabled }
+            onClick={ this.handleClick }
           >
             Jogar
           </button>
@@ -70,4 +82,10 @@ class Login extends Component {
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect()(Login);
