71a4ffcf458d6f4d275b3edf9cfb3ccf81f67a5c
Daniel Kojak <daniel.kojak@gmail.com>
2023-02-10 10:40:58 -0300

    Requisito 04 finalizado

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import Login from './pages/login';
-import Game from './pages/game';
-import Settings from './pages/settings';
+import Login from './pages/Login';
+import Game from './pages/Game';
+import Settings from './pages/Settings';
 
 export default function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/game" component={ Game } />
-        <Route path="/settings" component={ Settings } />
+        <Route path="/Game" component={ Game } />
+        <Route path="/Settings" component={ Settings } />
         <Route exact path="/" component={ Login } />
       </Switch>
     </div>
