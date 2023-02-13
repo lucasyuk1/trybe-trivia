95f4d0d74cc37ebce3b657fb180ffa66d5f63f11
Thalita <thalitacostasantos12@gmail.com>
2023-02-13 16:41:30 -0300

    Redirecionamento em andamento

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+// import { Redirect } from 'react-router-dom';
 import { incrementQuest, incrementScore } from '../redux/actions';
 
 class Questions extends Component {
